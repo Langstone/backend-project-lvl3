63ffcffb58bc630ae2eb90efd6c8807cb93d21e2
Langstone <kalipsomart@gmail.com>
2022-03-14 20:05:05 +0600

    added index.js, rename main page_loader.js to index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+import downloaderImages from './downloader_images.js';
+import downloaderPage from './downloader_page.js';
+import downloaderFiles from './downloader_files.js';
+import Listr from 'listr';
+
+export default function pageLoader(url, options) {
+  const tasks = new Listr([
+    {
+      title: 'Загрузка страницы',
+      task: ctx => downloaderPage(ctx.url, ctx.output)
+        .then(path => ctx.path = path)
+        .catch(err => {
+          console.error(`${err.name} ${err.message}`);
+        })
+    },
+    {
+      title: 'Загрузка изображений',
+      task: ctx => downloaderImages(ctx.url, ctx.path)
+        .then(nameForDirectory => ctx.nameForDirectory = nameForDirectory)
+        .catch(err => {
+          console.error(`${err.name} ${err.message}`);
+        })
+      },
+    {
+      title: 'Загрузка остальных ресурсов',
+      task: ctx => downloaderFiles(ctx.path, ctx.nameForDirectory, ctx.url)
+        .catch(err => {
+          console.error(`${err.name} ${err.message}`);
+        })
+    }
+  ]);
+  
+  tasks.run({
+    url: url,
+    output: options.output,
+  }).then(ctx => {
+    console.log(ctx.path)
+  }).catch(err => {
+    console.error(`${err.name} ${err.message}`);
+    console.log('Что-то пошло не так...')
+    process.exit(1);
+  });
+}
