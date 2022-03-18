238415a3bb1fc7105d9c32ac677a71b3dc617c63
Langstone <kalipsomart@gmail.com>
2022-03-18 16:12:15 +0600

    test_run_9_2

--- downloader_files.js
+++ downloader_files.js
@@ -73,6 +73,7 @@ const writeFile = (nameForDir, pathsList, url) => {
         url: src,
       })
         .then(answer => {
+          logPageLoader(answer.data);
           logPageLoader(`Получен ответ от ${src}`);
           const form = (src) => path.parse(src).ext === '' ? '.html' : path.parse(src).ext; 
           const format = form(src);
